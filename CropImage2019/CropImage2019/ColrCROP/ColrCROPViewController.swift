23d8faba6df7c894515ee6af8a7cf646e2379277
B35T <chaloemphong.ccs55@ubru.ac.th>
2019-04-06 16:16:41 +0700

    update

--- CropImage2019/CropImage2019/ColrCROP/ColrCROPViewController.swift
+++ CropImage2019/CropImage2019/ColrCROP/ColrCROPViewController.swift
@@ -340,19 +340,19 @@ extension ColrCROPViewController {
                 }
 
             default:
-                let multi_h = w * ratioCalculate
-
+                
                 if ratio.height >= ratio.width {
+                    let multi = w * ratioCalculate
                     let max_y =  multi + Grid.frame.origin.y
                     if max_y <= max.maxY && x <= max.maxX && multi >= minimumCrop {
                         view.center.x = x
                         self.Grid.frame.size.width = w
-                        self.Grid.frame.size.height = multi_h
+                        self.Grid.frame.size.height = multi
                     }
                 } else {
                     let multi = w / ratioCalculate
                     let max_y =  multi + Grid.frame.origin.y
-                    if max_y <= max.maxY && x <= max.maxX && multi_h >= minimumCrop {
+                    if max_y <= max.maxY && x <= max.maxX && multi >= self.minimumCrop {
                         view.center.x = x
                         self.Grid.frame.size.width = w
                         self.Grid.frame.size.height = multi
